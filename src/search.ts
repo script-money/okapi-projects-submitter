53a134eb093a5b32e7b42b2670f9384bb17c4a89
script-money <hello@script.money>
2024-02-26 20:00:58 +0800

    :sparkles: Search from twitterscore directly

--- src/search.ts
+++ src/search.ts
@@ -1,9 +1,26 @@
-import { keywordList } from "./config";
+import { twitterScoreUrl } from "./constant";
 import type { ProjectProps } from "./interface";
 
+interface HotProjectsDataProps {
+  data: {
+    profile_name: string;
+  }[];
+}
+
+let keywordList: string[] = [];
 let finalList: string[] = [];
 
 (async () => {
+  const hotProjects = await fetch(twitterScoreUrl, {
+    method: "GET",
+  });
+
+  const hotProjectsData = (await hotProjects.json()) as HotProjectsDataProps;
+  for (const project of hotProjectsData.data) {
+    keywordList.push(project.profile_name.split(" ")[0]);
+  }
+  console.log(keywordList);
+
   keywordList.forEach(async (keyword) => {
     const res = await fetch("https://app.okapi.xyz/api/project/search", {
       method: "POST",
@@ -18,8 +35,9 @@ let finalList: string[] = [];
     const resJson = (await res.json()) as ProjectProps;
 
     if (resJson.payload.projects?.length != null) {
-      console.log(resJson.payload.projects[0].name, " found");
+      // console.log(resJson.payload.projects[0].name, " found");
     } else {
+      console.log(keyword, "not found");
       finalList.push(keyword);
     }
     await new Promise((resolve) => setTimeout(resolve, 2000));
